b8cdaef508eae4ad34f1b92b203d8c1333f093c3
sttarrynight <suwo5612@naver.com>
2025-04-22 19:03:38 +0900

    Initial commit

---
+++ src/main/java/com/example/newscussbe/NewscussBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.newscussbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewscussBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewscussBeApplication.class, args);
+    }
+
+}
